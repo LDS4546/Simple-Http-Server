c73ef84b3d4e2826bceacb3fae9a9e14f0b29e0c
nhn-academy-marco <marco@ nhnacademy.com>
2024-04-12 17:47:51 +0900

    copyright 추가

--- src/main/java/com/nhnacademy/http/SimpleHttpServer.java
+++ src/main/java/com/nhnacademy/http/SimpleHttpServer.java
@@ -31,9 +31,14 @@ public class SimpleHttpServer {
 
             //TODO#9threadA를 생성하고 시작 합니다.
 
+            Thread threadA = new Thread(httpRequestHandlerA);
+            threadA.start();
+
+
 
             //TODO#10threadB를 생성하고 시작 합니다.
-
+            Thread threadB = new Thread(httpRequestHandlerB);
+            threadB.start();
 
             long count = 0;
 
@@ -42,6 +47,12 @@ public class SimpleHttpServer {
                 /*TODO#O11 count값이 짝수이면 httpRequestHandlerA에 client를 추가 합니다.
                            count값이 홀수라면 httpRequestHandlerB에 clinet를 추가 합니다.
                 */
+
+                if(count % 2 == 0){
+                    httpRequestHandlerA.addRequest(client);
+                }else{
+                    httpRequestHandlerB.addRequest(client);
+                }
 
                 count++;
             }
